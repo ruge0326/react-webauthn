7a83658089dbdd62fb0302833f825f5ed3bf9ef7
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-09-10 11:22:58 -0600

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [account, setAccount] = React.useState("");
+  const submit = () => {
+    console.log({ account });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Webauthn</h1>
+
+          {/* <form onSubmit={submit}>
+            <input>New account</input>
+            <input type="submit">Create</input>
+          </form> */}
+          <div>
+            <form>
+              <label htmlFor="account">Account</label>
+              <br></br>
+              <input
+                type="text"
+                id="account"
+                value={account}
+                onChange={(event) => {
+                  setAccount(event.target.value);
+                }}
+              />
+              <br></br>
+              <input type="button" value="Create Account" onClick={submit} />
+            </form>
+            <br></br>
+            {account && <div>Account: {account}</div>}
+          </div>
+        </div>
       </header>
     </div>
   );
